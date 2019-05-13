e70ebf714714876a89f10501b585548690fb2499
Squirrel18 <daniel.quiroga@edunext.co>
2019-05-13 11:34:54 -0500

    Adding time spent report backend.

--- get_settings.py
+++ get_settings.py
@@ -1,9 +1,18 @@
-import os
+"""
+Main module to get config settings from a file.
+"""
 import json
+import os
 
 
 def get_settings():
+    """
+    Returns and set environment settings in order to use them,
+    during the report fecth and generation.
 
+    Returns:
+        Dict with the settings defined in the config file.
+    """
     configuration_file_path = os.getenv('CONFIGURATION_FILE_PATH', None)
 
     if not configuration_file_path:
@@ -17,11 +26,18 @@ def get_settings():
         settings = json.loads(config_data)
         set_environment_settings(settings)
     except ValueError as json_error:
+        print(json_error.message)
         quit()
 
     return settings
 
 
 def set_environment_settings(settings):
+    """
+    Set environment keys from config file.
+
+    Args:
+        settings: Settings dict from the config file.
+    """
     os.environ['AWS_ACCESS_KEY_ID'] = settings.get('AWS_ACCESS_KEY_ID', '')
     os.environ['AWS_SECRET_ACCESS_KEY'] = settings.get('AWS_SECRET_ACCESS_KEY')
