92ac5188949a9a7cb1b9f001bcbf9d47572cc3ce
Squirrel18 <daniel.quiroga@edunext.co>
2019-05-17 12:41:47 -0500

    Adding support for cohort, teams, role and vertical position.

--- report_backend/last_page_accessed.py
+++ report_backend/last_page_accessed.py
@@ -20,7 +20,7 @@ class LastPageAccessedReportBackend(AbstractBaseReportBackend):
         super(LastPageAccessedReportBackend, self).__init__(**kwargs)
 
 
-    def json_report_to_csv(self, json_report_data={}):
+    def json_report_to_csv(self, json_report_data):
         """
         Process json data to convert into csv format.
         """
@@ -33,8 +33,15 @@ class LastPageAccessedReportBackend(AbstractBaseReportBackend):
         if report_data.get('last_page_data'):
             last_page_data = report_data.get('last_page_data')
             for course in last_page_data.keys():
-                last_page_report = self.last_page_accessed_report(course, last_page_data)
-                last_page_report_headers = ['username', 'last_time_accessed', 'last_page_viewed']
+                last_page_report = last_page_accessed_report(course, last_page_data)
+                last_page_report_headers = [
+                    'username',
+                    'user_cohort',
+                    'user_teams',
+                    'last_time_accessed',
+                    'last_page_viewed',
+                    'user_role',
+                ]
                 file_name = '{}-table'.format(course)
 
                 self.create_csv_file(file_name, last_page_report, last_page_report_headers)
@@ -42,11 +49,15 @@ class LastPageAccessedReportBackend(AbstractBaseReportBackend):
         if report_data.get('exit_count_data'):
             exit_count_data = report_data.get('exit_count_data')
             for course in last_page_data.keys():
-                exit_count_report = self.exit_count_report(course, exit_count_data)
-                exit_count_report_headers = ['page_title', 'exit_count',]
+                exit_count_report_data = exit_count_report(course, exit_count_data)
+                exit_count_report_headers = [
+                    'page_title',
+                    'exit_count',
+                    'position',
+                ]
                 file_name = '{}-bar-chart'.format(course)
 
-                self.create_csv_file(file_name, exit_count_report, exit_count_report_headers)
+                self.create_csv_file(file_name, exit_count_report_data, exit_count_report_headers)
 
 
     def create_csv_file(self, file_name, body_dict, headers):
@@ -86,49 +97,57 @@ class LastPageAccessedReportBackend(AbstractBaseReportBackend):
         )
 
 
-    def last_page_accessed_report(self, course, last_page_data):
-        """
-        Returns a csv dict to write the csv file.
-        """
-        if not last_page_data:
-            return {}
+def last_page_accessed_report(course, last_page_data):
+    """
+    Returns a csv dict to write the csv file.
+    """
+    if not last_page_data:
+        return {}
 
-        course_data = last_page_data.get(course, [])
-        csv_data = []
+    course_data = last_page_data.get(course, [])
+    csv_data = []
 
-        for user in course_data:
-            username = user.get('username', '')
-            last_time_accessed = user.get('last_time_accessed', '')
-            last_page_viewed = user.get('last_page_viewed', '')
+    for user in course_data:
+        username = user.get('username', '')
+        last_time_accessed = user.get('last_time_accessed', '')
+        last_page_viewed = user.get('last_page_viewed', '')
+        user_role = user.get('user_role', '')
+        user_cohort = user.get('user_cohort', '')
+        user_teams = user.get('user_teams', '')
 
-            dict_writer_data = {
-                'username': username,
-                'last_time_accessed': last_time_accessed,
-                'last_page_viewed': last_page_viewed,
-            }
-            csv_data.append(dict_writer_data)
+        dict_writer_data = {
+            'username': username,
+            'user_cohort': user_cohort,
+            'user_teams': user_teams,
+            'last_time_accessed': last_time_accessed,
+            'last_page_viewed': last_page_viewed,
+            'user_role': user_role,
+        }
+        csv_data.append(dict_writer_data)
 
-        return csv_data
+    return csv_data
 
 
-    def exit_count_report(self, course, exit_count_data):
-        """
-        Returns a csv dict to write the csv file.
-        """
-        if not exit_count_data:
-            return {}
+def exit_count_report(course, exit_count_data):
+    """
+    Returns a csv dict to write the csv file.
+    """
+    if not exit_count_data:
+        return {}
 
-        course_data = exit_count_data.get(course, [])
-        csv_data = []
+    course_data = exit_count_data.get(course, [])
+    csv_data = []
 
-        for block in course_data:
-            page_title = block.get('page_title', '')
-            exit_count = block.get('exit_count', '')
+    for block in course_data:
+        page_title = block.get('page_title', '')
+        exit_count = block.get('exit_count', '')
+        vertical_position = block.get('vertical_position', '')
 
-            dict_writer_data = {
-                'page_title': page_title,
-                'exit_count': exit_count,
-            }
-            csv_data.append(dict_writer_data)
+        dict_writer_data = {
+            'page_title': page_title,
+            'exit_count': exit_count,
+            'position': vertical_position,
+        }
+        csv_data.append(dict_writer_data)
 
-        return csv_data
+    return csv_data
