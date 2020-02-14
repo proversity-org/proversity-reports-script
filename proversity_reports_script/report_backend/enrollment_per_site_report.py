d57c304d7105c9bc67befeaf30f60114e0c6e3ad
Squirrel18 <daniel.quiroga@edunext.co>
2020-02-14 00:46:18 -0500

    Escaping delimiter comma character.

--- proversity_reports_script/report_backend/enrollment_per_site_report.py
+++ proversity_reports_script/report_backend/enrollment_per_site_report.py
@@ -98,7 +98,13 @@ class EnrollmentPerSiteReport(AbstractBaseReportBackend):
             return None
 
         with open(file_path, mode='w', encoding='utf-8') as csv_file:
-            writer = csv.DictWriter(csv_file, fieldnames=headers)
+            writer = csv.DictWriter(
+                csv_file,
+                fieldnames=headers,
+                quoting=csv.QUOTE_NONE,
+                doublequote=False,
+                escapechar=';',
+            )
 
             writer.writeheader()
 
